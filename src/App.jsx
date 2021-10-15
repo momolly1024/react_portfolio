b2e0a3090bf7e9ebb07b34c9327eb5a39a7bbbc5
Molly <momolly1024@gmail.com>
2021-10-15 15:00:09 +0800

    1015/temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Tntro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
 
 function App() {
     return (
         <div className="App">
             <Tntro />
             <About/>
+            <ProductList/>
+            <Contact/>
         </div>
     );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?cs=srgb&dl=pexels-negative-space-169573.jpg&fm=jpg",
+        link: "https://github.com/momolly1024",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?cs=srgb&dl=pexels-pixabay-270348.jpg&fm=jpg",
+        link: "https://github.com/momolly1024",
+    },
+    {
+        id: 3,
+        img: "",
+        link: "https://github.com/momolly1024",
+    },
+    {
+        id: 4,
+        img: "",
+        link: "https://github.com/momolly1024",
+    },
+    {
+        id: 5,
+        img: "",
+        link: "https://github.com/momolly1024",
+    },
+    {
+        id: 6,
+        img: "",
+        link: "https://github.com/momolly1024",
+    },
+];
